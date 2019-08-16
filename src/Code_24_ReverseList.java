abc01f34a98aa2a0df41cf5d51669c9882e2a0a9
meiko <260885777@qq.com>
2019-08-16 11:40:21 +0800

    题目二十四：反转链表

---
+++ src/Code_24_ReverseList.java
@@ -0,0 +1,56 @@
+/**
+ * 题目二十四：反转链表
+ * 定义一个函数，输入一个链表的头节点，反转该链表并输出反转后链表的头节点
+ *
+ * @author Jackie
+ * @date 2019/8/16
+ */
+public class Code_24_ReverseList {
+
+    private static class Node {
+        int key;
+        Node next;
+
+        public Node(int key) {
+            this.key = key;
+        }
+    }
+
+    private static Node reverseList(Node head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+
+        Node reverseHead = null;
+        Node cur = head;
+        Node pre = null;
+        while (cur != null) {
+            Node next = cur.next;
+
+            if (next == null) {
+                reverseHead = cur;
+            }
+
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+
+        return reverseHead;
+    }
+
+    public static void main(String[] args){
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
+        head.next.next.next.next.next = new Node(6);
+        head = reverseList(head);
+        while (head != null) {
+            System.out.print(head.key + " ");
+            head = head.next;
+        }
+    }
+
+}
