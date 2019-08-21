7c3e9c7fa8f01eeb7c53d248bd9d61a68b75e51f
meiko <260885777@qq.com>
2019-08-21 14:39:11 +0800

    题目三十八：字符串的排列

--- src/Code_38_StringPermutation.java
+++ src/Code_38_StringPermutation.java
@@ -3,7 +3,7 @@ import java.util.Set;
 
 /**
  * 题目三十八：字符串的排列
- * 输入一个字符串，打印出该字符串中姊夫的所有排列。
+ * 输入一个字符串，打印出该字符串中字符的所有排列。
  * 例如，输入字符串abc，则打印出由字符a、b、c所能排列出来的所有字符串abc、acb、bac、bca、cab和cba
  *
  * @author Jackie Ke
